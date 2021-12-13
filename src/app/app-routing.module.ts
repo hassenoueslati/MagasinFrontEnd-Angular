a7b1fb51a22bdb03e72128a9643cff9fd34402be
jihene7 <jihene.sliti@esprit.tn>
2021-12-13 23:41:09 +0100

    updatepf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { HomeComponent } from './home/home.component';
-
-
-import { StockComponent } from './stock/stock.component';
-import { ReclamationComponent } from './reclamation/reclamation.component';
-import { RayonComponent } from './rayon/rayon.component';
-
-import { DetailProduitComponent } from './detail-produit/detail-produit.component';
-import {MainUserComponent} from "./User/main-user/main-user.component";
-import {LoginComponent} from "./User/login/login.component";
-import {ProduitMainComponent} from "./produit-main/produit-main.component";
-import {FournisseurMainComponent} from "./fournisseur-main/fournisseur-main.component";
-import {RegisterUserComponent} from "./User/register/register-user.component";
-
-import { DepartementComponent } from './departement-management/departement/departement.component';
-import { DepartementMainComponent } from './departement-management/departement-main/departement-main.component';
-
-
-
-
-
-
-
-
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
- 
-  {path: 'user', component: MainUserComponent},
-  {path: 'produit', component: ProduitMainComponent},
-  {path: 'stock', component: StockComponent},
-  {path: 'reclamation', component: ReclamationComponent},
-  {path: 'rayon', component: RayonComponent},
-  {path: 'fournisseur', component: FournisseurMainComponent},
-  {path: 'detailProduit', component: DetailProduitComponent},
-  
- 
-  {path: 'register',component: RegisterUserComponent },
   { path: 'FactureManagement', loadChildren: () => import('./facture-management/facture-management.module').then(m => m.FactureManagementModule) },
-  { path: 'DepartementManagement', loadChildren: () => import('./departement-management/departement-management.module').then(m => m.DepartementManagementModule) }
+  { path: 'DepartementManagement', loadChildren: () => import('./departement-management/departement-management.module').then(m => m.DepartementManagementModule) },
+  { path: 'ProductManagement', loadChildren: () => import('./product-management/product-management.module').then(m => m.ProductManagementModule) },
+  { path: 'FournisseurManagement', loadChildren: () => import('./fournisseur-management/fournisseur-management.module').then(m => m.FournisseurManagementModule) },
+  { path: 'UserManagement', loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule) },
+  { path: 'RayonManagement', loadChildren: () => import('./rayon-management/rayon-management.module').then(m => m.RayonManagementModule) },
+  { path: 'StockManagement', loadChildren: () => import('./stock-management/stock-management.module').then(m => m.StockManagementModule) },
+  { path: 'ReclamationManagement', loadChildren: () => import('./reclamation-management/reclamation-management.module').then(m => m.ReclamationManagementModule) }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,27 +10,9 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { WapperComponent } from './wapper/wapper.component';
-
 import { SidebarWrapperComponent } from './sidebar-wrapper/sidebar-wrapper.component';
-
-
-
-import { ProduitComponent } from './produit/produit.component';
-import { StockComponent } from './stock/stock.component';
-import { ReclamationComponent } from './reclamation/reclamation.component';
-import { RayonComponent } from './rayon/rayon.component';
-import { FournisseurComponent } from './fournisseur/fournisseur.component';
-import { DetailProduitComponent } from './detail-produit/detail-produit.component';
-import { MainUserComponent } from './User/main-user/main-user.component';
-import { ListUserComponent } from './User/list-user/list-user.component';
 import {HttpClient, HttpClientModule} from "@angular/common/http";
-import { LoginComponent } from './User/login/login.component';
-import {ProduitMainComponent} from "./produit-main/produit-main.component";
-import {ProduitFormAddComponent} from "./produit-form-add/produit-form-add.component";
-import {FournisseurFormAddComponent} from "./fournisseur-form-add/fournisseur-form-add.component";
-import {FournisseurMainComponent} from "./fournisseur-main/fournisseur-main.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterUserComponent } from './User/register/register-user.component';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
 import {MatOptionModule} from "@angular/material/core";
@@ -62,32 +44,9 @@ const appRoutes: Routes = [
     HeaderComponent,
     FooterComponent,
     WapperComponent,
-
     SidebarWrapperComponent,
   
-    ProduitComponent,
-    StockComponent,
-    ReclamationComponent,
-    RayonComponent,
-    FournisseurComponent,
-    DetailProduitComponent,
- 
-    MainUserComponent,
-    ListUserComponent,
-    LoginComponent,
-    ProduitMainComponent,
-    ProduitFormAddComponent,
-    FournisseurFormAddComponent,
-    FournisseurMainComponent,
-    RegisterUserComponent,
-
-  
     
- 
-  
-   
-  
-
   ],
   imports: [
     BrowserModule,
@@ -119,6 +78,5 @@ const appRoutes: Routes = [
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents:[LoginComponent]
 })
 export class AppModule { }
